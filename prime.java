642055ec83badc5bf94da5e6d5f1a4317639ba0e
Asutosh <asutosh.panda@gmail.com>
2026-03-24 13:12:57 +0530

    Add Java prime number code

---
+++ prime.java
@@ -0,0 +1,19 @@
+public class Prime {
+    public static void main(String[] args) {
+        int n = 100; // Number limit
+        System.out.println("Prime numbers up to " + n + ":");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
